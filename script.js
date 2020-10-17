2ce4dbda15b0307fe182239f010f5dd1aa00247f
Suhail Malik <devmsuhail@gmail.com>
2020-10-17 18:58:36 +0530

    Fix height of portions for high resolution screens

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 window.onload = function () {
-    // or enable responsive functionality:
-    GitHubCalendar(".calendar", "suhailmalik07", { responsive: true });
+  // or enable responsive functionality:
+  GitHubCalendar(".calendar", "suhailmalik07", { responsive: true });
 }
